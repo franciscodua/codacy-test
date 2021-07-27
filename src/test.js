bc4cfbabd2e3d1030455649f7a9592f462d5082c
Francisco <francisco@codacy.com>
2021-07-27 13:52:26 +0100

    Change stuff

---
+++ src/test.js
@@ -0,0 +1,11 @@
+// the hello world program
+console.log('Hello World');
+
+console.log('Hello Universe');
+
+console.log('Hello other stuff');
+
+// TODO: ola
+// TODO [engine:node@>=8]: We can use async/await now.
+// FIXME [+read-pkg]: If we use this package we don't need to use this function below.
+// TODO [vue@>=3]: Refactor to function API when it's stable.
